c096591be675cfbb71835b2536c4825c77f7704f
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-02-19 09:12:22 +1100

    Add GrowableArray

--- foreach_in_interface/Program.cs
+++ foreach_in_interface/Program.cs
@@ -9,6 +9,30 @@ namespace foreach_in_interface
 {
     class GrowableArray : IEnumerable
     {
-        
+        object[] a;
+        public GrowableArray(int size)
+        {
+            a = new object[size];
+        }
+        public GrowableArray() : this(8) { }
+        void Grow()
+        {
+            object[] b = new object[2 * a.Length];
+            Array.Copy(a, b, a.Length);
+            a = b;
+        }
+        public object this[int i]
+        {
+            set
+            {
+                if (i >= a.Length) Grow();
+                a[i] = value;
+            }
+            get
+            {
+                if (i >= a.Length) Grow();
+                return a[i];
+            }
+        }
     }
 }
